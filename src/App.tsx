e317c0ef06cc55813709ea99cfa75b9894aeff18
Vlad Fedorov <79852880+Vlad19982602@users.noreply.github.com>
2024-10-14 10:44:19 +0500

    modified postcss and talwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,30 @@
-import React from 'react'
-import Calculator from './components/calculator/Calculator'
-import Header from './components/header/Header'
-import Sidebar from './components/sidebar/Sidebar'
-import Subheader from './components/Subheader/Subheader'
-import VentilationSystems from './components/ventilationSystem/VentilationSystems'
-import './style/style.css'
+import React from "react";
+import Calculator from "./components/calculator/Calculator";
+import Header from "./components/header/Header";
+import Sidebar from "./components/sidebar/Sidebar";
+import Subheader from "./components/subheader/Subheader";
+import VentilationSystems from "./components/ventilationSystem/VentilationSystems";
+import "./style/style.css";
 
 const App: React.FC = () => {
-	return (
-		<>
-			<div>
-				<Header />
-				<Subheader />
-				<div style={{ display: 'flex', justifyContent: 'space-between' }}>
-					<Sidebar />
-					<Sidebar />
-					<Sidebar />
-					<Sidebar />
-				</div>
-				<div className='main-content'>
-					<main>
-						<VentilationSystems />
-						<Calculator />
-					</main>
-				</div>
-			</div>
-		</>
-	)
-}
+  return (
+    <React.Fragment>
+      <Header />
+      <Subheader />
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Sidebar />
+        <Sidebar />
+        <Sidebar />
+        <Sidebar />
+      </div>
+      <div className="main-content">
+        <main>
+          <VentilationSystems />
+          <Calculator />
+        </main>
+      </div>
+    </React.Fragment>
+  );
+};
 
-export default App
+export default App;
